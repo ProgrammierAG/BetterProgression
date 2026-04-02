13b6fe9feae0d0f3ddd6c7650c37639f2d683413
steinemeister <oskar.hettel@web.de>
2026-04-02 11:05:04 +0200

    worked on Skilltree UI

--- src/client/java/better_progression/UIs/SkillTree.java
+++ src/client/java/better_progression/UIs/SkillTree.java
@@ -1,17 +1,13 @@
 package better_progression.UIs;
 
-import net.minecraft.client.Minecraft;
+import better_progression.BetterProgression;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
-
-import java.awt.*;
 
 public class SkillTree extends Screen {
 
@@ -22,20 +18,9 @@ public class SkillTree extends Screen {
     @Override
     protected void init() {
         // Buttons:
-//        Button squareButton = Button.builder(Component.empty(), button -> {
-//                    // Hier kommt die Aktion rein, die beim Klick ausgeführt wird
-//                    System.out.println("Button wurde gedrückt!");
-//                })
-//                .bounds(this.width / 2 - 10, this.height / 2 - 10, 20, 20)// x, y, Breite, Höhe
-//                .tooltip(Tooltip.create(Component.literal("Tooltip!")))
-//
-//                .build();
-//
-//        this.addRenderableWidget(squareButton);
-        Identifier BOOK_SPRITE = Identifier.fromNamespaceAndPath("minecraft", "textures/item/book.png");
-        // Da wir ein leeres Widget-Sprites-Objekt übergeben, verschwindet der graue Hintergrund
-        WidgetSprites iconSprites = new WidgetSprites(BOOK_SPRITE, BOOK_SPRITE);
 
+        Identifier TEST_SPRIT = Identifier.fromNamespaceAndPath(BetterProgression.MOD_ID, "test");
+        WidgetSprites iconSprites = new WidgetSprites(TEST_SPRIT, TEST_SPRIT);
         ImageButton squareButton = new ImageButton(
                 this.width / 2 - 10, this.height / 2 - 10, 20, 20,
                 iconSprites,
@@ -43,9 +28,7 @@ public class SkillTree extends Screen {
                     System.out.println("Button wurde gedrückt!");
                 }
         );
-
         squareButton.setTooltip(Tooltip.create(Component.literal("Tooltip!")));
-
         this.addRenderableWidget(squareButton);
 
         super.init();
